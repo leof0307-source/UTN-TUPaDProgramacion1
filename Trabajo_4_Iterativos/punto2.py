bae3b98d94ca01293ea2e71a5a3729e769952ca9
leof0307-source <leofach006@gmail.com>
2025-09-04 13:52:36 -0300

    4Puntos
    adjunto 4 puntos del trabajo practico 4

---
+++ Trabajo_4_Iterativos/punto2.py
@@ -0,0 +1,4 @@
+numero=int(input("ingrese un numero"))
+
+cantidad_digitos = len(str(numero))
+print(cantidad_digitos)
---
+++ Trabajo_4_Iterativos/punto4.py
@@ -0,0 +1,6 @@
+sumatoria=0 
+numero=int(input("ingrese un numero:"))
+while numero!=0:
+    sumatoria +=numero
+    numero=int(input("ingrese un numero:"))
+print("la sumatorio de todos estos numeros es=", + sumatoria)
